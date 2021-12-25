b581509d9a5044339aed56553e6001e3f0ae1e28
maliakos <maliakos@AntonioPC.localdomain>
2021-12-25 02:19:49 +0200

    Finished the requirements, and styling .Optinal requirement left

--- brain.js
+++ brain.js
@@ -1,35 +1,58 @@
 const drawingBoard = document.querySelector(".drawing-container");
-//Primitive functionality no argument.Small steps//
-drawingBoard.setAttribute(
-  "style",
-  "grid-template-columns:repeat(16,1fr);grid-template-rows(16,1fr);"
-);
+const resetButton = document.querySelector(".clear");
+const submitButton = document.getElementById("submit");
+let boardNodes;
+let squaresNumber = 16;
 
-function drawBoard() {
-  for (i = 0; i < 16 * 16; i++) {
+function drawBoard(squaresNumber) {
+  drawingBoard.setAttribute(
+    "style",
+    `grid-template-columns:repeat(${squaresNumber},1fr);`
+  );
+  for (i = 0; i < squaresNumber ** 2; i++) {
     newDiv = document.createElement("div");
     drawingBoard.appendChild(newDiv);
   }
+  boardNodes = document.querySelectorAll(".drawing-container div");
 }
 
+//Function to make the drawing board change colours on hover//
+function interactiveBoard() {
+  boardNodes.forEach((boardNode) =>
+    boardNode.addEventListener("mouseover", (e) => {
+      e.target.classList.add("paint-it");
+    })
+  );
+}
 
-function eraseBoard(){
-    boardNodes.forEach((boardNode) =>{
-    boardNode.classList.remove('paint-it-yellow');
+function eraseBoard() {
+  boardNodes.forEach((boardNode) => {
+    boardNode.classList.remove("paint-it");
   });
 }
+function resetBoard() {
+  while (drawingBoard.firstChild) {
+    drawingBoard.removeChild(drawingBoard.firstChild);
+  }
+  drawBoard(squaresNumber);
+  interactiveBoard();
+}
 
+//Initial 16 squares drawing board
+drawBoard(squaresNumber);
+interactiveBoard();
+/////////////////////////////////////////
 
+resetButton.addEventListener("click", eraseBoard);
 
-drawBoard();
-let boardNodes = document.querySelectorAll(".drawing-container div");
-console.log(boardNodes);
-//Colouring behaviour//
-boardNodes.forEach((boardNode) =>
-  boardNode.addEventListener("mouseover", (e) => {
-    e.target.classList.add('paint-it-yellow');
-  })
-);
-//Reset button//
-const resetButton=document.querySelector('#clear');
-resetButton.addEventListener('click',eraseBoard);
+submitButton.addEventListener("click", (e) => {
+  if (document.getElementById("squares_number").value) {
+    eraseBoard();
+    squaresNumber = document.getElementById("squares_number").value;
+    resetBoard();
+  }
+  // if (document.getElementById("darken_mode").checked) {
+  //   eraseBoard();
+
+  // }
+});
